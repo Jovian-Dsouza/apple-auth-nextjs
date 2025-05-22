ebedf38013e65bd1adbf5bea8883c9092c35e6df
Jovian Dsouza <dsouzajovian123@gmail.com>
2025-05-22 12:56:03 +0530

    feat: add apple auth api

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NextAuth from 'next-auth';
+import AppleProvider from 'next-auth/providers/apple';
+
+export default NextAuth({
+  providers: [
+    AppleProvider({
+      clientId: process.env.APPLE_CLIENT_ID!,
+      clientSecret: process.env.APPLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      (session as any).accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
